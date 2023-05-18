44a5ff4c3e34e31fa75d6f6d6b257d1f3022ad31
Terry Lamb <terrylamb@gmail.com>
2023-05-18 12:51:47 -0400

    create house file and test

---
+++ spec/house_spec.rb
@@ -0,0 +1,18 @@
+require './lib/room'
+require './lib/house'
+
+RSpec.describe House do
+    it 'exists and has readable attributes' do
+        house = House.new("$400000", "123 sugar lane")
+
+        expect(house).to be_a House
+        expect(house.price).to eq("$400000")
+        expect(house.address).to eq("123 sugar lane")
+    end
+    it 'has rooms' do
+        room_1 = Room.new(:bedroom, 10, '13')
+        room_2 = Room.new(:bedroom, 11, '15')    
+
+        expect(house.rooms).to eq []
+    end
+end
